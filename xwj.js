fd323bcfdfb673e05f7ecb6c4f52c48a127fa971
meetclover <107290487+meetclover@users.noreply.github.com>
2023-08-08 16:26:51 +0800

    Update xwj.js

--- xwj.js
+++ xwj.js
@@ -9,7 +9,7 @@
 const $ = new Env("西五街");
 let envSplitor = ['\n','@']     //多账号隔开方式
 let Notify = 1;               //通知开关，0为关,1为开
-let kami = 'meetUcHfOLTPcgftaeJgORh5XFsROQBz';     //卡密
+let kami = '';     //卡密
 
 let paiappver = '34102'
 let paichannel = 'vivoStore'
