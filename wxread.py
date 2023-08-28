0bf8de8e7fce890a5b08a0d2ec76192d5632c113
meetclover <107290487+meetclover@users.noreply.github.com>
2023-08-28 12:47:28 +0800

    微信阅读

--- wxread.py
+++ wxread.py
@@ -5,6 +5,7 @@ new Env('微信阅读');
 地址: http://dst1ya655g.qqaas.fun/app/main?openId=oiDdr50cvXog64PlMEvSfy3V31Hs#/
 微信捉包 http://xxxxxx/read/get 域名请求头里的 cookie
 
+注意: 每天第一轮跑之前先手动看大概10篇，遇到2-3次检测文章后再用脚本 
 青龙变量 export wxread="authtoken=eyJ0eXAiOiJKV1QiLCJhbxxxxx; snapshot=0" 多账号@隔开
 """
 import requests
@@ -52,8 +53,9 @@ class miniso:
     @staticmethod
     def setHeaders(i):
         headers = {
-            # "Referer": "http://dmohw3np0l.qqaas.fun/app/main?openId=oiDdr50cvXog64PlMEvSfy3V31Hs",
             "X-Requested-With": "com.tencent.mm",
+            "Content-Type": "application/json",
+            'Referer': 'https://umm92ibf7ldw99lk-1318684421.cos.ap-nanjing.myqcloud.com/',
             "User-Agent": "Mozilla/5.0 (Linux; Android 12; V2203A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5197 MMWEBSDK/20230405 MMWEBID/9296 MicroMessenger/8.0.35.2360(0x2800235D) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
             "Cookie": cookies[i],
         }
@@ -62,7 +64,7 @@ class miniso:
     @staticmethod
     def geturl(headers):
         try:
-            url = f'http://nfs7y9jp8w.qqaas.fun/app/read/get'
+            url = f'http://nqyt2plpvv.qqaas.fun/app/read/get'
             response = requests.get(url=url, headers=headers)
             result = response.json()
             id = re.search(r'u=(\w+)', result['data']['location']).group(1)
@@ -70,18 +72,6 @@ class miniso:
             logger.info(res)
             log_list.append(res)
             time.sleep(5)
-            miniso.read(headers, id)
-        except Exception as e:
-            print(e)
-
-    @staticmethod
-    def read(headers, id):
-        try:
-            url = f'https://32k1bs1abppkgvpv-1318684421.cos.ap-nanjing.myqcloud.com/read.html?u={id}'
-            response = requests.get(url=url, headers=headers)
-            res = f"模拟过检测... 等待10秒"
-            logger.info(res)
-            time.sleep(11)
             miniso.doRead(headers, id)
         except Exception as e:
             print(e)
@@ -100,7 +90,7 @@ class miniso:
                 time.sleep(8)
                 miniso.Read(headers, id, taskKey)
             else:
-                res = f"获取参数: 哦吼！已经没有阅读资格了！"
+                res = f"获取参数: 获取阅读参数失败！"
                 logger.info(res)
                 log_list.append(res)
         except Exception as e:
@@ -112,19 +102,17 @@ class miniso:
             url = f'https://sss.mvvv.fun/app/task/doRead?u={id}&type=1&key={taskKey}'
             response = requests.get(url=url, headers=headers)
             result = response.json()
-            logger.info(url)
-            logger.info(result)
             if result['data']['detail'] == '检测中':
                 miniso.doRead(headers, id)
-            elif result['code'] == 0:
+            elif result['code'] == 0 and result['data']['taskKey'] is not None:
                 taskKey = result['data']['taskKey']
-                sleeptime = random.randint(12, 25)
+                sleeptime = random.randint(8, 12)
                 res = f"阅读: {result['data']['detail']} -- 随机等待{sleeptime}秒后继续...."
                 logger.info(res)
                 time.sleep(sleeptime)
                 miniso.Read(headers, id, taskKey)
             else:
-                res = f"阅读: {result['msg']}"
+                res = f"阅读: 没获取到文章id,可能本轮阅读已完成 或 此账号已被限制!"
                 logger.info(res)
                 log_list.append(res)
         except Exception as e:
@@ -140,15 +128,20 @@ class miniso:
             print(e)
 
     @staticmethod
+    def myPickInfo(headers):
+        try:
+            url = f'http://qmctk1sfcw.qqaas.fun/app/user/myPickInfo'
+            response = requests.get(url=url, headers=headers)
+            return response.json()
+        except Exception as e:
+            print(e)
+
+    @staticmethod
     def pickAuto(headers, sign):
         try:
             data = sign
             url = 'http://mhxbn1se67.qqaas.fun/app/user/pickAuto'
             response = requests.post(url, headers=headers, data=data)
-            result = response.json()
-            print(response.request.body)
-            print(response.request.headers)
-            print(result)
             if result['code'] == 0:
                 res = f"提现: 拔毛成功！"
                 logger.info(res)
@@ -166,11 +159,12 @@ def encrypt(plaintext):
     ciphertext = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
     return b64encode(ciphertext).decode()
 
-
 if __name__ == '__main__':
     log_list = []  # 存储日志信息的全局变量
     for i in range(len(cookies)):
-        logger.info(f"\n------------开始第[{i + 1}]个账号------------")
+        head = f"\n------------开始第[{i + 1}]个账号------------"
+        logger.info(head)
+        log_list.append(head)
         headers = miniso.setHeaders(i)
 
         result = miniso.my(headers)
@@ -178,15 +172,15 @@ if __name__ == '__main__':
             res = f"账号: {result['data']['nameNick']} 今日已读:{result['data']['completeTodayCount']}次 金币:{result['data']['goldNow']}"
             logger.info(res)
             log_list.append(res)
-            if result['data']['completeTodayCount'] < 80:
+            if result['data']['remainSec'] == 0:
                 miniso.geturl(headers)
             else:
-                res = f"状态: 当前已不可阅读"
+                res = f"状态: 距离下次阅读还需{result['data']['remainSec']//60}分钟"
                 logger.info(res)
                 log_list.append(res)
-
             if result['data']['goldNow'] > 4000:
-                data = round(result['data']['goldNow'] / 10000 / 0.4) * 0.4
+                result = miniso.myPickInfo(headers)
+                data = result['data']['goldNow']
                 body = f'{{"moneyPick":{data:.1f}}}'
                 sign = encrypt(body)
                 miniso.pickAuto(headers,sign)
